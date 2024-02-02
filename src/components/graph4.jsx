15bead5315a519b6f12de327f0c3f4b7dc71def8
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2024-02-02 15:11:35 +0700

    minor update

--- src/components/graph4.jsx
+++ src/components/graph4.jsx
@@ -17,21 +17,21 @@ function Graph3(){
                 <div className="col-sm" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px', width: '100%'}}>
                         <div className="card-body p-0">
-                            <KSB61Flow url={'http://localhost:3030/real/flow-data?table=tb_ksb60'}/>
+                            <KSB61Flow url={'http://10.23.110.17:3030/real/flow-data?table=tb_ksb60'}/>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px', width: '100%'}}>
                             <div className="card-body p-0">
-                            <KSB61Pressure url={'http://localhost:3030/real/press-data?table=tb_ksb60'}/>
+                            <KSB61Pressure url={'http://10.23.110.17:3030/real/press-data?table=tb_ksb60'}/>
                             </div>
                         </div>
                     </div>
                 <div class="col-sm no-gutters" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px', width: '100%'}}>
                         <div className="card-body p-0">
-                            <KSB61Temp url={'http://localhost:3030/real/temp-data?table=tb_ksb60'}/>
+                            <KSB61Temp url={'http://10.23.110.17:3030/real/temp-data?table=tb_ksb60'}/>
                         </div>
                     </div>
                 </div>
@@ -40,21 +40,21 @@ function Graph3(){
                 <div class="col-sm" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px'}}>
                         <div className="card-body p-0">
-                            <KSB61EngineSpeed url={'http://localhost:3030/real/speed-data?table=tb_ksb60'}/>
+                            <KSB61EngineSpeed url={'http://10.23.110.17:3030/real/speed-data?table=tb_ksb60'}/>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px', width: '100%'}}>
                         <div className="card-body p-0">
-                            <KSB61EngineLoad url={'http://localhost:3030/real/load-data?table=tb_ksb60'}/>
+                            <KSB61EngineLoad url={'http://10.23.110.17:3030/real/load-data?table=tb_ksb60'}/>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
                     <div className="card" style={{backgroundColor: 'red500', height: '500px', width: '100%'}}>
                         <div className="card-body p-0">
-                            <KSB61EngineFuelRate url={'http://localhost:3030/real/fuel-data?table=tb_ksb60'}/>
+                            <KSB61EngineFuelRate url={'http://10.23.110.17:3030/real/fuel-data?table=tb_ksb60'}/>
                         </div>
                     </div>
                 </div>
