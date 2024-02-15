34d28a7aa87ba677695d6c6900f18558ba07eb0c
PC Engineering <engiadm@ksb.lan>
2024-02-15 11:39:10 +0700

    minor update

--- src/components/graph2.jsx
+++ src/components/graph2.jsx
@@ -23,24 +23,24 @@ function Graph2() {
             </div>
             <div className="row g-0 mt-2">
                 <div className="col-sm" style={{margin: 2}}>
-                    <KSB61Flow url={'http://localhost:3030/flow-data-date?table=tb_ksb61'}/>
+                    <KSB61Flow url={'http://ksb-iot.intranet:3030/flow-data-date?table=tb_ksb61'}/>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
-                    <KSB61Pressure url={'http://localhost:3030/press-data-date?table=tb_ksb61'}/>
+                    <KSB61Pressure url={'http://ksb-iot.intranet:3030/press-data-date?table=tb_ksb61'}/>
                 </div>
                 <div class="col-sm no-gutters" style={{margin: 2}}>
-                    <KSB61Temp url={'http://localhost:3030/temp-data-date?table=tb_ksb61'}/>
+                    <KSB61Temp url={'http://ksb-iot.intranet:3030/temp-data-date?table=tb_ksb61'}/>
                 </div>
             </div>
             <div class="row g-0">
                 <div class="col-sm" style={{margin: 2}}>
-                    <KSB61EngineSpeed url={'http://localhost:3030/speed-data-date?table=tb_ksb61'}/>
+                    <KSB61EngineSpeed url={'http://ksb-iot.intranet:3030/speed-data-date?table=tb_ksb61'}/>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
-                    <KSB61EngineLoad url={'http://localhost:3030/load-data-date?table=tb_ksb61'}/>
+                    <KSB61EngineLoad url={'http://ksb-iot.intranet:3030/load-data-date?table=tb_ksb61'}/>
                 </div>
                 <div class="col-sm" style={{margin: 2}}>
-                    <KSB61EngineFuelRate url={'http://localhost:3030/fuel-data-date?table=tb_ksb61'}/>
+                    <KSB61EngineFuelRate url={'http://ksb-iot.intranet:3030/fuel-data-date?table=tb_ksb61'}/>
                 </div>
             </div>
         </div>
